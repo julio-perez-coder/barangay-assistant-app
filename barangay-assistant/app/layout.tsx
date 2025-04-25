500a24dc6aeeedd46948699faea86b834d47eb5c
Julio Perez <jellezgod@gmail.com>
2025-04-25 23:45:09 +0800

    feat: add Navbar component for improved navigation

--- barangay-assistant/app/layout.tsx
+++ barangay-assistant/app/layout.tsx
@@ -29,7 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <Navbar /> {/* Add the Navbar component here */}
+        <Navbar />
         {children}
       </body>
     </html>
